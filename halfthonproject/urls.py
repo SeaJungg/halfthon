b40b1774b3d6acc38887030447d5c6f74d26fb36
Sea <wantedsaea@gmail.com>
2019-07-27 12:13:12 +0900

    메인페이지 띄우기

--- halfthonproject/urls.py
+++ halfthonproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import halfthonapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',halfthonapp.views.index, name='index'),
 ]
