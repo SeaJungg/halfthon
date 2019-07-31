4b2df83a91ad4bca853e0ffd586a26301a822362
Sea <wantedsaea@gmail.com>
2019-07-31 23:39:29 +0900

    goo plzz

--- halfthonproject/settings.py
+++ halfthonproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '50-28hzxz460uzb8ijv-t8kmw1fg_i0sb@r!ebfrf#stowmtm4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
